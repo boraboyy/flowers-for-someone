43b4f00d00974cea0ef245fd7676d8ba8bedb0e8
boraboyy <bora.yalcinkaya@hotmail.com>
2025-03-31 20:38:48 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Burda bişey var?'.split('')
+const text = 'Burda bişey var'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
