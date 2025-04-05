60626be92bfe1270145565ac5bf6f6db62b0c246
boraboyy <bora.yalcinkaya@hotmail.com>
2025-04-05 23:58:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Seni ÇOK seviyorum').split('')
+    const titles = ('Seni ÇOK seviyorum aşkım').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
